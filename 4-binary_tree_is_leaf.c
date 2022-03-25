1de74ae02354895ec1f57138df0628c9eeeeb9c3
Luijma <rsluismanuel@gmail.com>
2022-03-25 01:57:22 +0000

    t4

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - verifies node is part of tree
+ * @node: node being checked
+ * 
+ * Return: 1 if node is part of tree, else, 0
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return(0);
+
+	if (!node->right && !node->left)
+		return (1);
+
+	return (0);
+}
