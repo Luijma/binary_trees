c0b86f6cf29b0ab170695d73449b1a90568b0151
Luijma <rsluismanuel@gmail.com>
2022-03-25 02:06:17 +0000

    fixed betty

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -3,7 +3,6 @@
 /**
  * binary_tree_is_leaf - verifies node is part of tree
  * @node: node being checked
- * 
  * Return: 1 if node is part of tree, else, 0
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
